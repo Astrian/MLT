e9fb1448a54bad89385643b7d9798f99b272ffda
Astrian Zheng <happy.wheel6743@astrian.moe>
2022-09-13 19:49:03 +1000

    Add actions to button's trigger method

--- mlt-project/src/App.vue
+++ mlt-project/src/App.vue
@@ -1,9 +1,19 @@
 <template src="./index.html"></template>
 
 <script>
+    import axios from "axios"
     export default {
         name: 'App',
-
+        methods: {
+            async trigger() {
+                try {
+                    let res = await axios.get("http://localhost:8888")
+                    alert(res.data.message)
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        }
     }
 </script>
 
